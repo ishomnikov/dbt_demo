15193d30875d8b1c4709b337ed714b13a73c1242
Ivan Shomnikov <ivan.shomnikov@gmail.com>
2021-10-20 04:18:03 +0000

    completed staging tables

--- models/staging/stg_items.sql
+++ models/staging/stg_items.sql
@@ -1,1 +1,10 @@
-select * from {{ source('raw','raw_items') }}
+with source as (
+    select * from {{ source('raw','raw_items') }}
+)
+
+select 
+     id as item_id
+    ,cat as category
+    ,price 
+from source
+where id is not null
--- models/staging/stg_users.sql
+++ models/staging/stg_users.sql
@@ -1,1 +1,12 @@
-select * from {{ source('raw','raw_users') }}
+with source as (
+    select * from {{ source('raw','raw_users') }}
+) 
+
+select 
+     id as user_id
+    ,first as first_name
+    ,last as last_name
+    ,email
+    ,lang as language
+    ,deactivated
+from source
